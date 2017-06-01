a843eef5f3945322265d63530abd0c9d2f789f8c
arocketman <andrealfie@gmail.com>
2017-06-01 21:29:44 +0200

    Refactor login to custom component

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -11,17 +11,19 @@ function delete_cookie(name) {
     document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 }
 
+window.Event = new Vue();
 
-var vueLog = new Vue({
-    el : '#loggedIn',
-    data: {logged_in_msg : "",
-    isLoggedIn : false},
+Vue.component('login-component',{
+    template: '<a style="float:right" v-if="!isLoggedIn" href="/login">Login</a><p v-else id="loggedIn">{{logged_in_msg}} <span v-on:click="logOut" v-show="isLoggedIn"><b color="blue">Logout</b></span></p>',
+    data: function(){return {logged_in_msg : "",
+        isLoggedIn : false}},
     mounted(){
         if(! (getCookie("access_token") == null)){
             axios.get("/getUsername?access_token=" + getCookie("access_token"))
                 .then(function(response){
                     this.logged_in_msg = "Welcome back , " + response.data;
                     this.isLoggedIn = true;
+                    Event.$emit('logged-in');
                 }.bind(this))
                 .catch(function(error){
                     delete_cookie("access_token");
