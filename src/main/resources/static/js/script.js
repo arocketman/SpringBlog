caf037d9bded6c542908fd1a3ddbf0ec378bfc93
arocketman <andrealfie@gmail.com>
2017-06-02 17:48:51 +0200

    Minor adjustments

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -17,12 +17,12 @@ window.Event = new Vue({
 
 Vue.component('login-component',{
     template: '<ul class="navMenu" v-if="!isLoggedIn()" style="float:right"><li style="margin-right:5px"><a href="/login">Login</a></li><li><a href="/registration">Register</a></li></ul>' +
-    '<p v-else id="loggedIn">{{logged_in_msg}} <span v-on:click="logOut" v-show="isLoggedIn"><b color="blue">Logout</b></span></p>',
+    '<p v-else id="loggedIn">{{logged_in_msg}} <span v-on:click="logOut" v-show="isLoggedIn()"><b color="blue">Logout</b></span></p>',
     data: function(){
         return {logged_in_msg : ""}
     },
     mounted(){
-        if(! (getCookie("access_token") === null)){
+        if(getCookie("access_token")){
             axios.get("/getUsername?access_token=" + getCookie("access_token"))
                 .then(function(response){
                     this.logged_in_msg = "Welcome back , " + response.data;
